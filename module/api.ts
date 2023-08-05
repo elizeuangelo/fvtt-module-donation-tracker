fd04692d887afad76d5eea6b0183fb700ce02be8
elizeuangelo <elizeu90@gmail.com>
2023-08-05 15:42:46 -0300

    access more routes

--- module/api.ts
+++ module/api.ts
@@ -1,6 +1,6 @@
 import { getSetting } from './settings.js';
 
-type Permissions = 'query';
+type Permissions = 'query' | 'mutate' | 'admin';
 interface TokenData {
 	email: string;
 	exp: number;
@@ -37,12 +37,18 @@ export function isValid() {
 
 export function requestCode(email: string) {
 	const data = { id: game.user.id, email };
-	return fetch(getRoute('/login'), { method: 'POST', headers: getHeaders(), body: JSON.stringify(data) });
+	return fetch(getRoute('/login'), { method: 'POST', body: JSON.stringify(data) });
 }
 
 export function verifyCode(email: string, code: string) {
 	const data = { email, code };
-	return fetch(getRoute('/login'), { method: 'POST', headers: getHeaders(), body: JSON.stringify(data) });
+	return fetch(getRoute('/verify'), { method: 'POST', body: JSON.stringify(data) });
+}
+
+export async function myDonations() {
+	const manual = await (await fetch(getRoute('/manual/me'), { headers: getHeaders() })).json();
+	const kofi = await (await fetch(getRoute('/kofi/me'), { headers: getHeaders() })).json();
+	return { manual, kofi };
 }
 
 // -------------------------------------- //
@@ -52,7 +58,35 @@ export function isAdmin() {
 	return isValid() ? Boolean(payload?.perms) : false;
 }
 
-export function checkPermissions() {
+export function permissions() {
 	const payload = getTokenPayload();
 	return isValid() ? payload?.perms ?? null : null;
+}
+
+export function checkPermission(perm: Permissions) {
+	const perms = permissions();
+	if (!perms) return false;
+	return perms.includes(perm);
+}
+
+export async function allDonations() {
+	const manual = await (await fetch(getRoute('/manual/all'), { headers: getHeaders() })).json();
+	const kofi = await (await fetch(getRoute('/kofi/all'), { headers: getHeaders() })).json();
+	return { manual, kofi };
+}
+
+export async function serverVersion() {
+	return fetch(getRoute('/update/version'), { headers: getHeaders() });
+}
+
+export async function serverUpdate() {
+	return fetch(getRoute('/update/update'), { method: 'POST', headers: getHeaders() });
+}
+
+export async function serverConfig(config: Record<string, any>) {
+	return fetch(getRoute('/config'), { method: 'POST', headers: getHeaders(), body: JSON.stringify(config) });
+}
+
+export async function serverRestart() {
+	return fetch(getRoute('/restart'), { method: 'POST', headers: getHeaders() });
 }
